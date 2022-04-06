5a0feec559d568365e27611497e328ffc2c76e49
Stian Lystad <Stianlystad_1@hotmail.com>
2022-04-06 11:39:40 +0000

    initial setup

--- run.py
+++ run.py
@@ -1,3 +1,35 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random
+
+
+HANGMAN = [
+    '________',
+    '|       |',
+    '|       O',
+    '|       |',
+    '|      /|\ ',
+    '|       |',
+    '|      / \ '
+]
+
+WORDS = [
+    'PYTHON', 'JAVA', 'HTML', 'CSS', 'COMPUTER', 'LOTR',
+    'RONALDO', 'LAPTOP', 'BROWSER', 'LOGITECH', 'APPLE'
+]
+
+class Hangman():
+    """
+    Hangman game
+    """
+
+    def __init__(self,word_to_try):
+        self.failed_attempts = 0
+        self.word_to_try = word_to_try
+        self.game_progress = list('_' * len(self.word_to_try))
+
+    def locate_index(self,letter):
+        """
+        Takes a letter and returns a list with indexes in the word to guess
+        """
+        return [i for i, char in enumerate(self.word_to_try) if letter == char]
+    
+    
