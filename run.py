3b772810989aa955744b656a571cf51b6a5578a4
Stian Lystad <Stianlystad_1@hotmail.com>
2022-04-06 17:50:29 +0000

    make the game look better

--- run.py
+++ run.py
@@ -24,12 +24,21 @@ class Hangman():
     def __init__(self,word_to_try):
         self.failed_attempts = 0
         self.word_to_try = word_to_try
-        self.game_progress = list('_' * len(self.word_to_try))
+        self.game_progression = list('_' * len(self.word_to_try))
 
     def locate_index(self,letter):
         """
         Takes a letter and returns a list with indexes in the word to guess
         """
         return [i for i, char in enumerate(self.word_to_try) if letter == char]
-    
-    
+
+
+    def print_game(self):
+        """
+        Print the word to guess blankspaces remaining and attempts and guessed letters
+        """
+        print('\n')
+        print('\n'.join(HANGMAN[:self.failed_attempts]))
+        print('\n')
+        print(' '.join(self.game_progression))
+print_game(self)
