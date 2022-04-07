655540dec48c811933381e304cfe0bc7845a6c85
Stian Lystad <Stianlystad_1@hotmail.com>
2022-04-07 12:54:02 +0000

    fixed bug and added more structure

--- run.py
+++ run.py
@@ -18,50 +18,48 @@ WORDS = [
 
 class Hangman():
     """
-    Hangman game
+    The Hangman game
     """
 
     def __init__(self, word_to_try):
         self.failed_attempts = 0
         self.word_to_try = word_to_try
-        self.game_progression = list('_' * len(self.word_to_try))
+        self.game_progress = list('_' * len(self.word_to_try))
 
     def locate_indexes(self, letter):
         """
-        Takes a letter and returns a list with indexes in the word to guess
+        Method that takes a letter and returns a list with his indexes in
+        the word to try
         """
-        return [i for i, char in enumerate(self.word_to_try) if letter == char] 
+        return [i for i, char in enumerate(self.word_to_try) if letter == char]
 
     def is_invalid_letter(self, input_):
         """
         Method to validate if an user input is not just a letter
         """
-        return input_.isdigit() or (input.isalpha() and len(input) > 1)
-
+        return input_.isdigit() or (input_.isalpha() and len(input_) > 1)
 
     def print_game(self):
         """
-        Print the word to guess blankspaces remaining and attempts and guessed letters
+        Method to print the word to try blankspaces
         """
         print('\n')
         print('\n'.join(HANGMAN[:self.failed_attempts]))
         print('\n')
-        print(' '.join(self.game_progression))
+        print(' '.join(self.game_progress))
 
-    
     def update_prog(self, letter, indexes):
         """
-        Method to update Game progress with the guessed letters
+        Method to update the game progress with the guessed letters
         """
         for index in indexes:
-            self.game_progression[index] = letter
+            self.game_progress[index] = letter
 
     def get_user_input(self):
         user_input = input('\nType a letter: ')
         return user_input
-    
 
-    def game(self):
+    def play(self):
         """
         Def to play the game, asking user for letter and plays the game.
         """
@@ -69,11 +67,12 @@ class Hangman():
             self.print_game()
             user_input = self.get_user_input()
 
+
             if self.is_invalid_letter(user_input):
-                print('input not available')
+                print('Input not available')
                 continue
 
-            if user_input in self.game_progression:
+            if user_input in self.game_progress:
                 print('You have already guessed that')
                 continue
 
@@ -81,14 +80,13 @@ class Hangman():
                 indexes = self.locate_indexes(user_input)
                 self.update_prog(user_input, indexes)
 
-                if self.game_progression.count('_') == 0:
-                    print('\n You won!')
+                if self.game_progress.count('_') == 0:
+                    print('You won!')
                     print('The word is: {0}'.format(self.word_to_try))
                     quit()
             else:
                 self.failed_attempts += 1
-                print("\n You lost")
-
+        print("\n You lost")
 
 if __name__ == '__main__':
     word_to_try = random.choice(WORDS)
